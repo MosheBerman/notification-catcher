3f5385f09ae1a15ba22d9f9883fa321a549c7214
Moshe Berman <moshberm@gmail.com>
2013-08-12 11:21:35 -0400

    Initial Commit

---
+++ Catcher/NNViewController.h
@@ -0,0 +1,13 @@
+//
+//  NNViewController.h
+//  Catcher
+//
+//  Created by Moshe Berman on 8/12/13.
+//  Copyright (c) 2013 Moshe Berman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NNViewController : UIViewController
+
+@end
---
+++ CatcherTests/CatcherTests.m
@@ -0,0 +1,34 @@
+//
+//  CatcherTests.m
+//  CatcherTests
+//
+//  Created by Moshe Berman on 8/12/13.
+//  Copyright (c) 2013 Moshe Berman. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface CatcherTests : XCTestCase
+
+@end
+
+@implementation CatcherTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
